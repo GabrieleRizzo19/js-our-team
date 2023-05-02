9e1dcc498a4091073e1f2bc4208c93a3eaeec976
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-05-02 17:50:35 +0200

    milestone 1 and 2

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
-const team = [
+const our_team = [
 
-    {name: "Wayne Barnett", role: "Founder & CEO", img: "wayne-barnett-founder-ceo.jpg"},
-    {name: "Angela Caroll", role: "Chief Editor", img: "angela-caroll-chief-editor.jpg"},
-    {name: "Walter Gordon", role: "Office Manager", img: "walter-gordon-office-manager.jpg"},
-    {name: "Angela Lopez", role: "Social Media Manager", img: "angela-lopez-social-media-manager.jpg"},
-    {name: "Scott Estrada", role: "Developer", img: "scott-estrada-developer.jpg"},
-    {name: "Barbara Ramos", role: "Graphic Designer", img: "barbara-ramos-graphic-designer.jpg"}
+    {name: "Wayne Barnett", role: "Founder & CEO", img: "./img/wayne-barnett-founder-ceo.jpg"},
+    {name: "Angela Caroll", role: "Chief Editor", img: "./img/angela-caroll-chief-editor.jpg"},
+    {name: "Walter Gordon", role: "Office Manager", img: "./img/walter-gordon-office-manager.jpg"},
+    {name: "Angela Lopez", role: "Social Media Manager", img: "./img/angela-lopez-social-media-manager.jpg"},
+    {name: "Scott Estrada", role: "Developer", img: "./img/scott-estrada-developer.jpg"},
+    {name: "Barbara Ramos", role: "Graphic Designer", img: "./img/barbara-ramos-graphic-designer.jpg"}
 
 ]
 
-console.log(team);
+console.log(our_team);
+
+
+
+for(let i=0; i < our_team.length; i++){
+    for(let key in our_team[i]){
+        document.querySelector("body").innerHTML += `Valore: ${our_team[i][key]} <br>`;
+    }
+    document.querySelector("body").innerHTML += "<br>";
+}
